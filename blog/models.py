f950428b535ae5ea7092b6bcb7f5e3b7ccfbd4b9
craggiecraggie <craggie@tempmail.de>
2016-06-18 07:38:45 +0000

    Veritabanındaki postları görebilmek için template'i değiştirdim.

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 class Scklk(models.Model):
-    ver1 = models.CharField(max_length=80)
-    ver2 = models.CharField(max_length=80)
+    veris = models.CharField(max_length=80)
+    verin = models.CharField(max_length=80)
        
 class Hrkt(models.Model):
-    ver3 = models.CharField(max_length=80)
+    verih = models.CharField(max_length=80)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Scklk
+from .models import Hrkt
 # Create your views here.
+
 def post_list(request):     
+    posts = Hrkt.objects.all()
     return render(request, 'blog/post_list.html', {})
